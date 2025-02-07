edcf962009e48912a0c0b475eddff1b1eb2ecc85
planact-dev <shinsata1650@gmail.com>
2025-02-07 20:54:49 +0900

    Suspense 에러 수정

--- src/app/search/result/page.tsx
+++ src/app/search/result/page.tsx
@@ -8,6 +8,14 @@ import { useSearchParams } from 'next/navigation'
 import { Suspense, useEffect, useState } from 'react'
 
 export default function Page() {
+  return (
+    <Suspense>
+      <Container />
+    </Suspense>
+  )
+}
+
+const Container = () => {
   const [teachers, setTeachers] = useState<TeacherListDto[]>([])
 
   useEffect(() => {
@@ -21,7 +29,7 @@ export default function Page() {
   }
 
   return (
-    <Suspense>
+    <>
       <BackAppbar />
       <div className="w-full py-5 px-5 flex-col justify-start items-center gap-8 inline-flex">
         <div className="self-stretch  flex-col justify-start items-start gap-4 flex">
@@ -31,6 +39,6 @@ export default function Page() {
         </div>
         <AdvisorList advisorList={teachers} />
       </div>
-    </Suspense>
+    </>
   )
 }
