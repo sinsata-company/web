5b4f776bcdc6289d828b4599bcbc45040999f467
planact-dev <shinsata1650@gmail.com>
2025-02-12 16:56:18 +0900

    meta 태그 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,65 @@ export default function RootLayout({
         name="naver-site-verification"
         content="d5cbe4cc3ead7ff00b35d193bfa3ef45a1b08e87"
       />
+      <meta
+        name="naver-site-verification"
+        content="0c0d649acea321e0fc6765f7f2df6044d68b5d34"
+      />
+      <meta
+        name="google-site-verification"
+        content="_UKMZd5tDpoNaGWD7AtZ70PnqoYg367BAnGT57VV_-U"
+      />
+      <link
+        rel="shortcut icon"
+        href="https://www.sinsata.co.kr/images/favicon_.ico"
+      />
+      <link rel="canonical" href="https://www.sinsata.co.kr/" />
+      <meta name="robots" content="index,follow" />
+      <meta name="subject" content="신사타" />
+      <meta name="title" content="신사타" />
+      <meta name="author" content="신사타" />
+      <meta
+        name="description"
+        content="검증된 선생님들만 모신 오늘의운세, 띠별운세, 신사타에서 확인해 보세요."
+      />
+      <meta name="keywords" content="신사타" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:rich_attachment" content="true" />
+      <meta property="og:site_name" content="신사타" />
+      <meta property="og:title" content="신사타" />
+      <meta
+        property="og:description"
+        content="검증된 선생님들만 모신 오늘의운세, 띠별운세, 신사타에서 확인해 보세요."
+      />
+      <meta
+        property="og:image"
+        content="https://www.sinsata.co.kr/images/og.png"
+      />
+      <meta property="og:url" content="https://www.sinsata.co.kr/" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="신사타" />
+      <meta
+        name="twitter:description"
+        content="검증된 선생님들만 모신 오늘의운세, 띠별운세, 신사타에서 확인해 보세요."
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.sinsata.co.kr/images/og.png"
+      />
+
+      <meta itemProp="name" content="신사타" />
+      <meta
+        itemProp="description"
+        content="검증된 선생님들만 모신 오늘의운세, 띠별운세, 신사타에서 확인해 보세요."
+      />
+      <meta
+        itemProp="image"
+        content="https://www.sinsata.co.kr/images/og.png"
+      />
+      <title>신사타</title>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative w-full h-full max-w-[550px] mx-auto`}
       >
