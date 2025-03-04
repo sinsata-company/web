748230d50a1219d94570f6c95c86169503f31293
안세영 <ayh747474@naver.com>
2025-03-04 12:14:52 +0900

    feat: 상담사 로그인 예외처리 alert 추가 (심사이슈 대응)

--- src/app/manage/auth/login/page.tsx
+++ src/app/manage/auth/login/page.tsx
@@ -1,49 +1,49 @@
-'use client'
+'use client';
 
-import { Button, BUTTON_TYPE } from '@/components/common/Button'
-import Input from '@/components/common/Input'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { loginTeacher } from '../../api/auth'
+import { Button, BUTTON_TYPE } from '@/components/common/Button';
+import Input from '@/components/common/Input';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { loginTeacher } from '../../api/auth';
 
 export default function Page() {
-  const [pin, setPin] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+    const [pin, setPin] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const router = useRouter();
 
-  const router = useRouter()
-
-  const onClick = async () => {
-    await loginTeacher(pin, password)
-    router.replace('/manage/app/main/home')
-  }
-  return (
-    <div className="flex flex-col gap-8 w-full">
-      <Input
-        name="아이디"
-        placeholder="이메일을 입력해주세요"
-        value={pin}
-        onChange={(e) => {
-          setPin(e.target.value)
-        }}
-      />
-      <Input
-        name="비밀번호"
-        placeholder="이메일을 입력해주세요"
-        value={password}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            onClick()
-          }
-        }}
-        onChange={(e) => {
-          setPassword(e.target.value)
-        }}
-      />
-      <Button
-        buttonType={BUTTON_TYPE.primary}
-        label="로그인"
-        onClick={onClick}
-      />
-    </div>
-  )
+    const onClick = async () => {
+        try {
+            await loginTeacher(pin, password);
+        } catch (e) {
+            alert('로그인에 실패했습니다. 계정 정보를 확인해 주세요.');
+            return;
+        }
+        router.replace('/manage/app/main/home');
+    };
+    return (
+        <div className="flex flex-col gap-8 w-full">
+            <Input
+                name="아이디"
+                placeholder="이메일을 입력해주세요"
+                value={pin}
+                onChange={(e) => {
+                    setPin(e.target.value);
+                }}
+            />
+            <Input
+                name="비밀번호"
+                placeholder="이메일을 입력해주세요"
+                value={password}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        onClick();
+                    }
+                }}
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+            />
+            <Button buttonType={BUTTON_TYPE.primary} label="로그인" onClick={onClick} />
+        </div>
+    );
 }
