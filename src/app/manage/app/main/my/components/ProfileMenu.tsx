c52769a1d8bc5503dd7eeb22036acda7b626170e
sinsata-company <skg09203@naver.com>
2025-03-12 21:47:13 +0900

    lucide-react 아이콘 추가

--- src/app/manage/app/main/my/components/ProfileMenu.tsx
+++ src/app/manage/app/main/my/components/ProfileMenu.tsx
@@ -1,55 +1,77 @@
 import ProfileMenuItems from './ProfileMenuItems'
+import {
+  Album,
+  BookHeart,
+  CircleHelp,
+  Clock,
+  FileQuestion,
+  Hash,
+  ShoppingCart,
+  SquareDot,
+  UserMinus
+} from "lucide-react";
 
 export interface IMenuItem {
   route: string
   title: string
+  icon: any
 }
+const ICON_SIZE = 30;
 
 const ProfileMenu = () => {
   const menus: IMenuItem[] = [
     {
       route: 'time',
       title: '상담 가능 시간 설정',
+      icon: <Clock size={ICON_SIZE} />
     },
     {
       route: 'hashtag',
       title: '내 해시태그 관리',
+      icon: <Hash size={ICON_SIZE} />
     },
     {
       route: 'notice',
       title: '공지사항 작성',
+      icon:<Album size={ICON_SIZE} />
     },
     {
       route: 'intro',
       title: '내 소개 수정',
+      icon: <SquareDot size={ICON_SIZE} />
     },
     {
       route: 'strength',
       title: '잘하는 분야 수정',
+      icon: <BookHeart size={ICON_SIZE} />
     },
     {
       route: 'qna',
       title: '자주 묻는 질문',
+      icon: <CircleHelp size={ICON_SIZE} />
     },
     {
       route: 'va',
       title: '부가서비스',
+      icon: <ShoppingCart size={ICON_SIZE} />
     },
     {
       route: 'withdraw',
       title: '회원탈퇴',
+      icon: <UserMinus size={ICON_SIZE} />
     },
       {
       route: 'inquiry',
       title: '1:1 문의관리',
+        icon: <FileQuestion size={ICON_SIZE} />
     },
   ]
   return (
     <div className="w-full flex-col justify-start items-start inline-flex">
       <div className="grid grid-cols-3 gap-3 w-full my-6">
       {menus.map((menu, index) => (
-        <div key={index} className="border-gray-100 border-[1px] flex justify-center items-center p-0 shadow-md">
-          <ProfileMenuItems route={menu.route} title={menu.title} />
+        <div key={index} className="flex justify-center items-center p-0">
+          <ProfileMenuItems icon={menu.icon} route={menu.route} title={menu.title} />
         </div>
       ))}
     </div>
--- src/app/manage/app/main/my/components/ProfileMenuItems.tsx
+++ src/app/manage/app/main/my/components/ProfileMenuItems.tsx
@@ -1,52 +1,55 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import { IMenuItem } from './ProfileMenu'
+import {useRouter} from 'next/navigation'
+import {IMenuItem} from './ProfileMenu'
 import Modal from '@/components/common/Modal'
-import { useState } from 'react'
-import { Button, BUTTON_TYPE } from '@/components/common/Button'
+import {useState} from 'react'
+import {Button, BUTTON_TYPE} from '@/components/common/Button'
 
-const ProfileMenuItems = ({ route, title }: IMenuItem) => {
-  const router = useRouter()
-  const [showModal, setShowModal] = useState(false)
-  return (
-    <div
-      onClick={() => {
-        if (route == 'withdraw') {
-          console.log('탈퇴하기')
-          setShowModal(true)
-        } else {
-          router.push('/manage/app/my/' + route)
-        }
-      }}
-      className="cursor-pointer p-5 bg-white flex-col justify-center items-start gap-2.5 inline-flex"
-    >
-      <div className="self-stretch h-full text-black text-base font-normal  leading-relaxed">
-        {title}
-      </div>
-      <Modal
-        isOpen={showModal}
-        onClose={(e) => {
-          e.stopPropagation()
+const ProfileMenuItems = ({route, title, icon}: IMenuItem) => {
+    const router = useRouter()
+    const [showModal, setShowModal] = useState(false)
+    return (
+        <div
+            onClick={() => {
+                if (route == 'withdraw') {
+                    console.log('탈퇴하기')
+                    setShowModal(true)
+                } else {
+                    router.push('/manage/app/my/' + route)
+                }
+            }}
+            className="cursor-pointer bg-white flex-col justify-center items-center gap-2.5 inline-flex mb-5"
+        >
+            <div className="rounded-lg flex justify-center items-center bg-gray-200 p-3">
+                {icon}
+            </div>
+            <div className="self-stretch h-full text-black text-base font-normal  leading-relaxed">
+                <p>{title}</p>
+            </div>
+            <Modal
+                isOpen={showModal}
+                onClose={(e) => {
+                    e.stopPropagation()
 
-          setShowModal(false)
-          console.log(showModal)
-        }}
-        title="회원탈퇴"
-        content="탈퇴 후에는 복구되지 않습니다. 하단 버튼을 눌러 카카오톡 상담을 이용하신 후 절차에 따라 회원탈퇴를 안내해드리겠습니다."
-      >
-        <Button
-          buttonType={BUTTON_TYPE.primary}
-          onClick={() => {
-            window.open('http://pf.kakao.com/_rMFxbn', '_blank')
-            // 탈퇴 API 호출
-            setShowModal(false)
-          }}
-          label="탈퇴하기"
-        />
-      </Modal>
-    </div>
-  )
+                    setShowModal(false)
+                    console.log(showModal)
+                }}
+                title="회원탈퇴"
+                content="탈퇴 후에는 복구되지 않습니다. 하단 버튼을 눌러 카카오톡 상담을 이용하신 후 절차에 따라 회원탈퇴를 안내해드리겠습니다."
+            >
+                <Button
+                    buttonType={BUTTON_TYPE.primary}
+                    onClick={() => {
+                        window.open('http://pf.kakao.com/_rMFxbn', '_blank')
+                        // 탈퇴 API 호출
+                        setShowModal(false)
+                    }}
+                    label="탈퇴하기"
+                />
+            </Modal>
+        </div>
+    )
 }
 
 export default ProfileMenuItems
