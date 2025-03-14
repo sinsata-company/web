a578598208f74eabd47d671fc393d6cc9dc1fdf8
hanyongwoo <nasms88@naver.com>
2025-03-15 02:00:30 +0900

    components 수정

--- src/app/home/components/AdvisorContainer.tsx
+++ src/app/home/components/AdvisorContainer.tsx
@@ -5,7 +5,7 @@ import AdvisorList from './AdvisorList'
 import AdvisorSort from './AdvisorSort'
 import { TeacherListDto } from '@/app/api/data'
 import { getTeacherList, SearchType } from '@/app/api/teacher'
-import { useSearch } from '@/components/common/SearchContext'
+import { useSearch } from 'd'
 
 // 초성 추출 함수
 const getChosung = (str: string) => {
