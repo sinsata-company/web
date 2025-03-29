35010a4926ebebf7eafe3cb92c8a5672aa3dd240
sinsata-company <shinsata1650@gmail.com>
2025-03-30 01:18:35 +0900

    client id 변경

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -85,7 +85,7 @@ export default function RegisterPage() {
     // 환경에 따른 클라이언트 ID 설정
     //const clientId = isIOS 
 
-    const clientId = 'com.sst.sinsataCs'
+    const clientId = 'sinsataCs.ssst.com'
     const redirectUri = BASE_WEB + '/register/oauth/apple'
     const appleAuthUrl =
       `https://appleid.apple.com/auth/authorize?` +
