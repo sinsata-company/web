3ed7a8e3467bacd147d893b13234c45cccccbfb3
sinsata-company <shinsata1650@gmail.com>
2025-03-30 23:51:59 +0900

    부가서비스 결제금액 수정

--- src/app/api/http/mtn.ts
+++ src/app/api/http/mtn.ts
@@ -5,16 +5,22 @@ export const getPayURL = async (
   timestamp: string
 ): Promise<string> => {
   const user = await getMyInfo()
+  
+  // 부가세 10% 계산
+  const vatAmount = Math.floor(amount * 0.1)  // 부가세 계산 (소수점 버림)
+  const totalAmount = amount + vatAmount      // 총 결제 금액
+  
   const mtonetUrl = `https://passcall.co.kr:32837/cptl/gnrc-mob/pay
           ?membid=${user?.mtnId}
           &cpid=0035
-          &coinamt=${amount}
-          &amount=${amount}
+          &coinamt=${amount}         // 원래 금액
+          &amount=${totalAmount}     // 부가세 포함된 총 금액
           &telno=${user?.phoneNum}
           &membnm=${encodeURIComponent(user?.name)}
           &item=${encodeURIComponent(`신사타 코인 ${amount}원`)}
           &oid=${timestamp}
-          &returnurl=${encodeURIComponent('https://demo.apple-login-test.app:8080/api/v1/mtn/payment/request')}
-          &formurl=${encodeURIComponent('https://demo.apple-login-test.app:8080/api/v1/mtn/payment/payment-complete')}`
+          &returnurl=${encodeURIComponent('https://api.sinsata.co.kr/api/v1/mtn/payment/request')}
+          &formurl=${encodeURIComponent('https://api.sinsata.co.kr/api/v1/mtn/payment/payment-complete')}`
+          
   return mtonetUrl.replace(/\s/g, '')
 }
